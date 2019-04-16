d1d62bf56f22e8a1c8fb9a0bcf4e999e71a0dc9c
kyberdrb <kyberdrb@gmail.com>
2019-04-16 08:40:17 +0200

    Convert input arguments via atoi

--- gcd.cpp
+++ gcd.cpp
@@ -17,13 +17,19 @@ int main(int argc, char **argv) {
     }
 
     // TODO load numbers as arguments from terminal
-    const int numberOfElements = 4;
+
+    const int numberOfElements = argc;
+
     // TODO check every calloc/malloc/realloc function for null pointer
     int* numbers = (int*) calloc(numberOfElements, sizeof(int));
-    numbers[0] = 10;
-    numbers[1] = 20;
-    numbers[2] = 30;
-    numbers[3] = 40;
+
+    int convertedNumber = 0;
+    for (int j = 1; j < numberOfElements; j++) {
+        // TODO encapsulate conversion into a separate class
+        convertedNumber = atoi(argv[j]);
+        std::cout << convertedNumber << std::endl;
+        numbers[j-1] = convertedNumber;
+    }
 
     std::unique_ptr<GcdFinder> gcdFinder(new GcdFinder());
     int maxGcd = gcdFinder->find_max_gcd(numbers, numberOfElements);
